e80fbdb16cf0c5a1f9280b818a01c4db24b6797c
Sombode <25levyle@gmail.com>
2024-10-27 01:52:19 -0400

    Initial cleanup

--- build.sh
+++ build.sh
@@ -1,16 +1,8 @@
-#!/bin/sh
-
-# Jim and Adriweb
-
-echo "Building FormulaPro Nspire v1.42b..."
+echo "Building FormulaPlus"
 
 echo "Building the database"
 cd 0\ -\ \ Database
 ./build.sh
-
-#cd ..
-#cd 1\ -\ \ Analysis\ Part
-#./build.sh
 
 echo "Building the FormulaPro core"
 cd ..
@@ -22,23 +14,20 @@ cd ..
 cd 3\ -\ \ Reference\ Part
 ./build.sh
 
-echo "Building the libraries"
+echo "Building Libraries"
 cd ..
 cd Global\ Libraries
 ./build.sh
 
 cd ..
 
-echo "Creating the whole thing..."
-#cat Database.big.lua lib.big.lua FormulaPro.big.lua Analysis.big.lua Reference.big.lua main.lua > EEPro.big.lua
-cat Database.big.lua lib.big.lua FormulaPro.big.lua Reference.big.lua main.lua > EEPro.big.lua
-mv EEPro.big.lua FormulaPro_fullcode.lua
-luna FormulaPro_fullcode.lua FormulaPro.tns
-cp FormulaPro.tns EEPro.tns
+echo "Putting it all together"
+cat Database.big.lua lib.big.lua FormulaPro.big.lua Reference.big.lua main.lua > FormulaPlus.big.lua
+./luna/luna FormulaPlus.big.lua FormulaPlus.tns
 
 echo "Done building"
 
 echo "Cleaning up"
 rm *.big.lua
 
-echo "Done. Enjoy !"
+echo "Done. Enjoy!"
